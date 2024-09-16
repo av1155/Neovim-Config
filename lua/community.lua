3e94188a8f6b4e838b158f81be8684a43bba5cba
av1155 <117413846+av1155@users.noreply.github.com>
2024-09-16 12:38:38 -0400

    feat (copilot): enabled markdown filetype copilot suggestions

--- lua/community.lua
+++ lua/community.lua
@@ -337,7 +337,7 @@ return {
             },
             filetypes = {
                 yaml = false,
-                markdown = false,
+                markdown = true,
                 help = false,
                 gitcommit = false,
                 gitrebase = false,
