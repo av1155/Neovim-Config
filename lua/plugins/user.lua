f3cd86501773f523bb5fa873c41fd1171ec9d0b7
av1155 <117413846+av1155@users.noreply.github.com>
2024-09-19 21:31:39 -0400

    fixed cursor when using code runner

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -188,7 +188,7 @@ return {
         opts = {
             mode = "toggleterm",
             focus = true,
-            startinsert = true,
+            startinsert = false,
             filetype = {
                 -- Scripting Languages
                 python = "cd $dir && python -u $fileName",
